c19d94f2a1050d696442f0bea767f96d19361a9f
70Qiang <strongcoderman@gmail.com>
2024-09-11 18:03:37 +0800

    http  请求啊  有点问题 临时提交版本

---
+++ entry/src/main/ets/model/ShopModel.ts
@@ -0,0 +1,54 @@
+import ShopInfo from '../viewmodel/ShopInfo'
+import http from '@ohos.net.http';
+import { JSON } from '@kit.ArkTS';
+import { BusinessError } from '@kit.BasicServicesKit';
+
+class ShopModel{
+  baseURL:string = 'http://localhost:3000'
+  pageNo:number = 1
+
+  getShopList():Promise<ShopInfo[]>{
+
+    return new Promise((resolve,reject)=>{
+
+      let httpRequest = http.createHttp()
+
+      httpRequest.request(
+        `${this.baseURL}/shops?pageNo=${this.pageNo}&pageSize=3`,
+        {
+          method:http.RequestMethod.GET
+        }
+      )
+        .then( (resp:http.HttpResponse) => {
+
+          if(resp.responseCode === 200){
+
+            console.log('查询成功',resp.result)
+            const result = JSON.parse(resp.result as string) as ShopInfo[]
+
+            console.log('成功的result ',)
+
+            resolve(result)
+          }else {
+            reject('查询失败')
+          }
+
+        })
+        .catch( (error:BusinessError) =>{
+          console.log('失败',error.message)
+
+        })
+
+    })
+
+
+
+
+  }
+
+}
+
+
+const  shopModel = new ShopModel();
+
+export  default shopModel as ShopModel
